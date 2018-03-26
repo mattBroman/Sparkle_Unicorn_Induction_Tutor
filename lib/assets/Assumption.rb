ccb2a2e97caf5b51c88ecfda85ecc3bd9f2157d0
ryan.hennigan@tamu.edu <ryan.hennigan@tamu.edu>
2018-03-26 02:23:20 +0000

    added eqeval

--- lib/assets/Assumption.rb
+++ lib/assets/Assumption.rb
@@ -2,12 +2,11 @@ require "json"
 
 class Assumption
    
-   
-   
    def initialize(args)
         
         #non empty assumption
         @assumptions = JSON.parse(args)["assumptions"]
+        raise RuntimeError, "Invalid JSON request" unless not @assumptions.nil?
         raise RuntimeError, "No base cases provided" unless not @assumptions.empty?
 
 
@@ -17,7 +16,16 @@ class Assumption
                 raise RuntimeError, "Invalid base variable" unless (key.to_s.match(/^[A-Za-z]$/) && key.to_s.length == 1)
             end
         end
+        
+        puts @assumptions
 
+   end
+   
+   
+   def evaluate
+      
+      @assumptions
+       
    end
     
     
---
+++ lib/assets/EqExpression.rb
@@ -0,0 +1,71 @@
+require "json"
+require_relative 'Evaluate.rb'
+
+class EqExpression 
+    
+    
+    
+    
+    
+    def initialize(args, assumptions)
+        
+        @eqBlocks = JSON.parse(args)["equivalenceExpressions"]
+        
+        raise RuntimeError, "Invalid JSON input" unless not @eqBlocks.nil?
+        
+        @assumptions = assumptions
+        
+        #add assumptions to eq's
+        @assumptions.evaluate.each do |assumption|
+            assumption.each do |key,val|
+                @eqBlocks.each do |eqLine|
+                        eqLine["left"].map! { |x|
+                            x==key.to_s ? val.to_s : x
+                        }
+                        eqLine["right"].map! { |x|
+                            x==key.to_s ? val.to_s : x
+                        }
+                end
+            end
+        end
+        
+        
+    
+        
+       
+        #p @eqLine
+        
+        
+    end
+    
+    
+    def evaluate
+       eval = Evaluator.new
+        
+        val = eval.solve(@eqBlocks[0]["left"])
+        
+       @eqBlocks.each do |eqLine|
+            if(not(val == eval.solve(eqLine["left"]))) then
+                return false
+            end
+            
+            if not eqLine["right"].empty? then
+                
+                if(not(val==eval.solve(eqLine["right"]))) then
+                    return false
+                end
+                
+            end    
+           
+       end
+        
+        
+        return true
+        
+    
+    end
+    
+end
+
+    
+    
