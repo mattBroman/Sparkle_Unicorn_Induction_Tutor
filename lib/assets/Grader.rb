2865e5e26ad057ed9864ddfe93caaccb98aeca6b
ryan.hennigan@tamu.edu <ryan.hennigan@tamu.edu>
2018-03-27 02:52:53 +0000

    added Grader rspec and improved evaluate for Grader

--- lib/assets/Grader.rb
+++ lib/assets/Grader.rb
@@ -3,19 +3,40 @@ class Grader
     
     #pass json of problem here
    def initialize(args)
-    
-       # p args.to_s
+        
+        @exception = nil
+        
+        begin
           
-      @bc = BaseCase.new(args)
+            @bc = BaseCase.new(args)
+        
+        rescue Exception => exc
+            @exception = true
+            @bc_exception = exc
+        end
+        
+        #add inductive hypothesis and what not here later...
       
-      #add inductive hypothesis and what not here later...
-       
        
        
    end
     
     def evaluate
-        @bc.evaluate
+        
+        #return any errors or the correct evaluation
+        
+        if !@exception.nil? then
+            
+            @bcval = @bc_exception.message unless @bc_exception.nil?
+            
+        else
+            @bcval = @bc.evaluate
+            
+            
+        end
+        
+        {:baseCase => @bcval}
+        
     end
     
     
