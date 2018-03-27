5306c045a5dc549f4d78d5db649d54069652e741
EC2 Default User <matthew.broman@yahoo.com>
2018-03-27 03:12:12 +0000

    parser outputs correcting and sends result of meaningful parse to server

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,9 +21,9 @@ var data = 'default';
 window.onkeyup = function parse() {
   let text = document.getElementById('plain_text').value;
   try {
-    PARSER.parse(text);
-    document.getElementById('pretty_page').value = 'Good!';
-    document.getElementById('hidden').value = '{\"parse\":\"Good!\"}'
+    let result = JSON.stringify(PARSER.parse(text));
+    document.getElementById('pretty_page').value = result;
+    document.getElementById('hidden').value = result;
     console.log('here')
   }
   catch(error) {
