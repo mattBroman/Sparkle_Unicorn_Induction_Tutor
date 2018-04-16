91c3742a0773b8f814f42cd28921e3ef43c67c84
EC2 Default User <ec2-user@ip-172-31-6-180.us-east-2.compute.internal>
2018-04-16 00:40:13 +0000

    progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,3 @@ class SessionsController < ApplicationController
   def destroy
   end
 end
-
-
-class SessionsController < ApplicationController
-  def create 
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = use.id
-    redirect_to root_path
-  end
-    
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path
-  end
-    
-end
